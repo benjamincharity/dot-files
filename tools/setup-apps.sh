adcad48e00206e860eaf6cdcd1b98b0e9eef7b24
Benjamin Charity <ben@benjamincharity.com>
2015-06-27 15:04:00 -0400

    added quicklook plugins, removed chrome/dropbox/1password

--- tools/setup-apps.sh
+++ tools/setup-apps.sh
@@ -4,8 +4,8 @@ echo "Brew Cask:"
 brew tap caskroom/cask
 brew install brew-cask
 
-echo "iTerm2:"
-brew cask install iterm2
+echo "Quicklook Plugins:"
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
 echo "Alfred:"
 brew cask install alfred
@@ -36,70 +36,97 @@ brew cask install pixelapse
 
 echo "VLC:"
 brew cask install vlc
+
 echo "Caffine:"
 brew cask install caffine
+
 echo "JumpCut:"
 brew cask install jumpcut
-echo "Chrome:"
-brew cask install google-chrome
+
 echo "Github:"
 brew cask install github
+
 echo "Creative Cloud:"
 brew cask install adobe-creative-cloud
-echo "1Password:"
-brew cask install 1password
+
 echo "CloudApp:"
 brew cask install cloud
+
 echo "TotalSpace:"
 brew cask install totalspaces
+
 echo "SkyFonts:"
 brew cask install monotype-skyfonts
+
 echo "Karabiner:"
 brew cask install karabiner
+
 echo "Seil:"
 brew cask install seil
+
 echo "BetterTouchTool:"
 brew cask install bettertouchtool
+
 echo "Sketch:"
 brew cask install sketch
+
 echo "Sketch Toolbox:"
 brew cask install sketch-toolbox
+
 echo "xScope:"
 brew cask install xscope
+
 echo "Slack:"
 brew cask install slack
+
 echo "MongoHub:"
 brew cask install jeromelebel-mongohub
+
 echo "Charles:"
 brew cask install charles
-echo "Visual Studio Code:"
-brew cask install visual-studio-code
+
+#echo "Visual Studio Code:"
+#brew cask install visual-studio-code
+
 echo "ImageOptim:"
 brew cask install imageoptim
+
 echo "Marked:"
 brew cask install marked
+
 echo "Plex Media Server:"
 brew cask install plex-media-server
+
 echo "Plex Home Theater:"
 brew cask install plex-home-theater
+
 echo "Noizio:"
 brew cask install noizio
+
 echo "Plug:"
 brew cask install plug
+
 echo "Amazon Music:"
 brew cask install amazon-music
+
 echo "Amazon Cloud Drive:"
 brew cask install amazon-cloud-drive
+
 echo "LimeChat:"
 brew cask install limechat
+
 echo "Transmission:"
 brew cask install transmission
+
 echo "Carbon Copy Cloner:"
 brew cask install carbon-copy-cloner
+
 echo "The Unarchiver:"
 brew cask install the-unarchiver
-echo "Dropbox:"
-brew cask install dropbox
+
+#echo "Dropbox:"
+#brew cask install dropbox
+
 echo "ngrok:"
 brew cask install ngrok
 
