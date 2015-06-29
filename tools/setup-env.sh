2f55e5e83d306782ec1565fad5178f3670ed530c
Benjamin Charity <ben@benjamincharity.com>
2015-06-29 07:09:49 -0400

    added .git_template to dot files, added git templates link to setup-env.sh

--- tools/setup-env.sh
+++ tools/setup-env.sh
@@ -13,20 +13,23 @@ echo "Cloning dot-files..."
 git clone git@github.com:benjamincharity/dot-files.git ~/.dot-files
 
 echo "Creating links..."
-echo ".zshrc"
+echo "Link .zshrc"
 ln -s ~/.dot-files/zshrc ~/.zshrc
 
-echo ".vim"
+echo "Link .vim"
 [[ -L ~/.vim ]] || ln -s ~/.dot-files/vim ~/.vim
 
-echo ".vimrc"
+echo "Link .vimrc"
 ln -s ~/.dot-files/vim/vimrc ~/.vimrc
 
-echo ".gvimrc"
+echo "Link .gvimrc"
 ln -s ~/.dot-files/vim/gvimrc ~/.gvimrc
 
-echo ".gitconfig"
+echo "Link .gitconfig"
 ln -s ~/.dot-files/gitconfig ~/.gitconfig
+
+echo "Link .git_template"
+ln -s ~/.dot-files/.git_template ~/.git_template
 
 echo "Create swap files dir"
 mkdir ~/.dot-files/vim/swap_files
